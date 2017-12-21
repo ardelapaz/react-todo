8f55eb58d9e40b5b922db5d846d23dc029c665c4
unknown <ardelapaz@yahoo.com>
2017-12-20 19:21:34 -0500

    test

---
+++ bloc-jams-jquery/scripts/album-info.js
@@ -0,0 +1,6 @@
+{
+$('#album-title').text(album.title);
+$('img#album-cover-art').attr('src', album.albumArtUrl);
+$('.artist').text(album.artist);
+$('#releaseInfo').text(album.releaseInfo);
+}
---
+++ bloc-jams-jquery/scripts/player-bar.js
@@ -0,0 +1,43 @@
+{
+    $('button#play-pause').on('click', function() {
+        player.playPause();
+        $(this).attr('playState', player.playState);
+    });
+
+    $('button#next').on('click', function() {
+        if (player.playState !== 'playing') { return; }
+
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const nextSongIndex = (currentSongIndex + 1);
+        if (nextSongIndex >= album.songs.length) { return; }
+        const nextSong = album.songs[nextSongIndex];
+        player.playPause(nextSong);
+    });
+
+    $('button#previous').on('click', function() {
+        if (player.playState !== 'playing') { return; }
+
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const prevSongIndex = (currentSongIndex - 1);
+        if (prevSongIndex <= -1) { return; }
+
+        const prevSong = album.songs[prevSongIndex];
+        player.playPause(prevSong);
+
+
+    });
+
+    setInterval( () => {
+        if (player.playerState !== 'playing') { return; }
+
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime / duration ) * 100;
+        $('#time-control input').val(percent);
+
+
+
+
+    }, 1000);
+    
+}
