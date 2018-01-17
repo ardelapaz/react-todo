e26f4c22afd5b53b6e2ae8bdb7380085d292dd3d
Andrew Delapaz <ardelapaz@yahoo.com>
2018-01-16 22:25:01 -0500

    Checkpoint 7

---
+++ Projects/bloc-jams-jquery/scripts/player-bar.js
@@ -0,0 +1,47 @@
+{
+    $('button#play-pause').on('click', function() {
+        player.playPause();
+        $(this).attr('playState', player.playState);
+    });
+
+    $('button#next').on('click', function() {
+        if (player.playState !== 'playing') { return; }
+
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const nextSongIndex = (currentSongIndex + 1);
+        if (nextSongIndex >= album.songs.length) { return; }
+        const nextSong = album.songs[nextSongIndex];
+        player.playPause(nextSong);
+    });
+
+    $('button#previous').on('click', function() {
+        if (player.playState !== 'playing') { return; }
+
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const prevSongIndex = (currentSongIndex - 1);
+        if (prevSongIndex <= -1) { return; }
+
+        const prevSong = album.songs[prevSongIndex];
+        player.playPause(prevSong);
+    });
+
+    setInterval( () => {
+        if (player.playState !== 'playing') { return; }
+
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime / duration ) * 100;
+        $('#time-control .current-time').text( Math.floor(currentTime) );
+        $('#time-control input').val(percent);
+        $('#time-control .total-time').text(duration);
+
+
+    }, 1000);
+    
+   $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+});
+$('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+});
+}
